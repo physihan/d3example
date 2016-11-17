32d45867ab403a27b4a3dd40f5100fc35ea166b3
费习函 <physihan@outlook.com>
2016-11-17 22:03:13 +0800

    柱状图堆叠显示，文字添加悬浮显示

--- d3-set.js
+++ d3-set.js
@@ -63,4 +63,59 @@
 //   :::::: B O D Y : :  :   :    :     :        :          :
 // ──────────────────────────────────────────────────────────
 //
+var dataset = [14, 46, 19, 63, 21, 29, 52, 2, 42, 92, 73, 36, 44, 22, 94, 89, 82, 51, 75, 80, 64, 37, 55, 97, 17, 77, 6, 32, 23, 45, 50]
+var dataset1 = [69, 58, 96, 13, 54, 11, 56, 14, 72, 49, 82, 91, 75, 71, 0, 29, 89, 83, 55, 37, 51, 84, 67, 95, 48, 76, 7, 15, 25, 20, 87]
+// var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ]
+var w = 800,
+  h = 200
+var barPadding = 2
+var mysvg = d3.select('body')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h)
+// const scale=d3.scale.linear()
+mysvg.selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append('rect')
+  // .attr('x', (d, i) => i * 21)
+  .attr('x', function (d, i) {
+    return i * (w / dataset.length)
+  })
+  .attr('y', d => h - d)
+  .attr('width', w / dataset.length - barPadding)
+  .attr('height', d => d)
+  .attr('fill', 'teal')
+  .on("mouseenter", (d,i)=>{return showtext(i)})
 
+
+mysvg.selectAll('.second')//这里要避免重名，这里是选择这一类来应用
+  .data(dataset1)
+  .enter()
+  .append('rect')
+  .attr('class','second')
+  .attr('x', (d, i) => i * w / dataset1.length)
+  .attr('y', (d, i) => h - d - dataset[i])
+  .attr('width', w / dataset.length - barPadding)
+  .attr('height', d => d)
+  .attr('fill', 'red')
+
+mysvg.selectAll('text')
+  .data(dataset)
+  .enter()
+  .append('text')
+  .text(d => d)
+  .attr('x', function (d, i) {
+    return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2
+  })
+  .attr('y', function (d) {
+    return h - d
+  })
+  .attr('text-anchor', 'middle')
+  .attr('opacity', 0)
+function showtext( i){
+  console.log(i)
+console.log(document.getElementsByTagName('text')[i])
+document.getElementsByTagName('text')[i].setAttribute('opacity',1)
+  console.log(this)
+}
